501673fe4b0564993067237e42276d7aa8f23c3f
Rfed0r0v <rf.rf944@gmail.com>
2022-06-21 21:22:16 +0300

    dev v1

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.weatherapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.watherapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textViewID = findViewById<TextView>(R.id.textViewId)
+        textViewID.text = "Text2"
     }
 }
