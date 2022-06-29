7e60de1ba03b222afcc83a916c27c5bcffb1ff86
Rfed0r0v <rf.rf944@gmail.com>
2022-06-29 21:44:32 +0300

    перед попыткой объедениться с веткой development

--- app/src/main/java/com/example/weatherapplication/features/weather_screen/data/WeatherAPI.kt
+++ app/src/main/java/com/example/weatherapplication/features/weather_screen/data/WeatherAPI.kt
@@ -6,6 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherAPI {
+
     @GET("weather")
     fun getWeather(
         @Query("q") query: String,
--- app/src/main/java/com/example/weatherapplication/features/weather_screen/data/WeatherAPIClient.kt
+++ app/src/main/java/com/example/weatherapplication/features/weather_screen/data/WeatherAPIClient.kt
@@ -6,14 +6,15 @@ import retrofit2.Retrofit
 
 object WeatherAPIClient {
     private val okHttpClient = OkHttpClient.Builder().build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(okHttpClient)
         .build()
 
     private val weatherAPI = retrofit.create(WeatherAPI::class.java)
-    fun getApi(): WeatherAPI {
 
+    fun getApi(): WeatherAPI {
         return weatherAPI
     }
 }
