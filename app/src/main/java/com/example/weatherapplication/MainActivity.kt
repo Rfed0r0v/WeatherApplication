0532ec0048df75a1cbaeeb1ce0e72f70df1602f4
Rfed0r0v <rf.rf944@gmail.com>
2022-09-01 21:55:18 +0300

    CollapsingToolbarLayout - завершено

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -9,11 +9,9 @@ import com.example.watherapplication.R
 import com.example.weatherapplication.features.weather_screen.ui.UiEvent
 import com.example.weatherapplication.features.weather_screen.ui.ViewState
 import com.example.weatherapplication.features.weather_screen.ui.WeatherScreenViewModel
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +19,21 @@ class MainActivity : AppCompatActivity() {
     private val textViewID: TextView by lazy { findViewById<TextView>(R.id.textViewId1) }
     private val fabWeather: FloatingActionButton by lazy { findViewById(R.id.fabWeatherFetch) }
     private val progressBar: ProgressBar by lazy { findViewById(R.id.progressBar) }
+    private val collapsingToolbar:CollapsingToolbarLayout by lazy{findViewById(R.id.collapsingToolbar)}
+    private val weatherAppBar:AppBarLayout by lazy {findViewById(R.id.weatherAppBar)}
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel.viewState.observe(this, ::render)
+
+        weatherAppBar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val percent = (kotlin.math.abs(appBarLayout.totalScrollRange + verticalOffset)
+                .toFloat() / appBarLayout.totalScrollRange)
+            fabWeather.alpha = percent
+        }
+
         fabWeather.setOnClickListener {
             viewModel.processUiEvent(UiEvent.OnButtonClicked)
         }
@@ -34,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun render(viewState: ViewState) {
         progressBar.isVisible = viewState.isLoading
+        collapsingToolbar.title = "Температура: ${viewState.temperature}"
         textViewID.text = "Температура: ${viewState.temperature}"
     }
 }
